9efb699f2d1bc59c22f54e969176abc5d1b3b5ab
Briandito <briandito.p@gmail.com>
2018-08-02 00:05:55 +0800

    finsihed parallax chapter

--- DesignCode/DesignCode-App/DesignCode-App/ViewController.swift
+++ DesignCode/DesignCode-App/DesignCode-App/ViewController.swift
@@ -56,7 +56,17 @@ class ViewController: UIViewController {
 extension ViewController: UIScrollViewDelegate{
     
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print(scrollView)
+        
+        let offsetY = scrollView.contentOffset.y
+        
+        if offsetY < 0 {
+            heroView.transform = CGAffineTransform(translationX: 0, y: offsetY)
+            playVisualEffectView.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            titleLabel.transform = CGAffineTransform(translationX: 0, y: -offsetY/3)
+            deviceImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/4)
+            backgroundImageView.transform = CGAffineTransform(translationX: 0, y: -offsetY/5)
+        }
+        
     }
     
 }
