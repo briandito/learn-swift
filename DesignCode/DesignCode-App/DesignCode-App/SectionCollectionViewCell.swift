2687250b597f0ccd790c5971851bcf4ac48a67af
Briandito <briandito.p@gmail.com>
2018-08-03 23:11:23 +0800

    added new class for cell view

---
+++ DesignCode/DesignCode-App/DesignCode-App/SectionCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SectionCollectionViewCell.swift
+//  DesignCode-App
+//
+//  Created by Briandito Priambodo on 03/08/18.
+//  Copyright © 2018 Briandito Priambodo. All rights reserved.
+//
+
+import UIKit
+
+class SectionCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+}
